e564da0137c671466434e6633f7f1917e072f122
shhun <calvin.abou-haidar@ens-lyon.fr>
2020-01-03 16:48:08 +0100

    added a training matrix generating function to test.py

--- test.py
+++ test.py
@@ -35,14 +35,18 @@ def train_matrix_from_file(path, sep=',', test_set={}):
             ...
             x, y, value
 
-        and a test_set consisting of line numbers of the matrix descripting
+        and a @test_set consisting of line numbers of the matrix describing
         file at @path that will be considered to be the test values.
 
-        It returns the matrix (called train_matrix)
+        It returns:
+            -the matrix (called @train_matrix)
         described by the file (where the test values are
-        set to be zero) and the vector
-        test_values of the form [(x, y, value),...] consisting of
-        the coordinates of the test values in the matrix and the values.
+        set to be zero)
+            - a vector @test_values of the form [(x, y, value),...] consisting
+            of the coordinates of the test values in the matrix and the values
+            - a vector @values [(x, y),...] containing the coordinates
+            and values of the non-test values (the training set) (ie the
+            non-zero values of the @train_matrix)
 
         (',' can be replaced by any separator string by changing the @sep
         parameter)
@@ -53,6 +57,7 @@ def train_matrix_from_file(path, sep=',', test_set={}):
 
         matrix = [[0 for _ in range(columns)] for _ in range(lines)]
         test_entries = []
+        values = []
 
         for (n, entry) in enumerate(fd.readlines()):
             # If the entry corresponds to a training value,
@@ -61,13 +66,24 @@ def train_matrix_from_file(path, sep=',', test_set={}):
             movie_id, user_id, rating = (int(x) for x in entry.split(sep))
             if n not in test_set:
                 matrix[movie_id][user_id] = rating
+                values.append((movie_id, user_id))
             else:
                 test_entries.append((movie_id, user_id, rating))
 
-    return matrix, test_entries
+    return matrix, test_entries, values
 
 
 def train_matrix(alpha, path='./data/matrix.txt', sep=','):
+    """
+        @alpha: the percentage of values of the matrix used as test values
+        @path: the path to the matrix describing file
+        @sep: the separator used in the file at @path
+
+        train_matrix returns a tuple (matrix, test_values, values)
+        where @matrix is the matrix used to train the recommender, that
+        is, the matrix described at @path minus the @test_values.
+        @values contains coordinates of the non-test values.
+    """
     with open(path, 'r') as fd:
         _, _, nbvalues = (int(x) for x in fd.readline().split(sep))
         test_set = generate_test(alpha, nbvalues)
